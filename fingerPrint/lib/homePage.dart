7cee3ed966783f467dcce20a394ede6ce05de234
Deepu Singla <52960031+AbhinandanSingla@users.noreply.github.com>
2022-10-17 14:30:20 +0530

    Add FingerPrint Scanner

---
+++ fingerPrint/lib/homePage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Text("HOME PAGe", style: GoogleFonts.openSans(fontSize: 30),),
+        ),
+      ),
+    );
+  }
+}
