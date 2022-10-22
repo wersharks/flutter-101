57e88ac7e021f41600dfff380731a8533dc38af9
ananyachawla1 <108053283+ananyachawla1@users.noreply.github.com>
2022-10-23 01:49:40 +0530

    Made some UI fixes including button style and text style

--- destini-app/lib/main.dart
+++ destini-app/lib/main.dart
@@ -44,6 +44,7 @@ class _StoryPageState extends State<StoryPage> {
                   child: Text(
                     //TODO: Step 10 - use the storyBrain to get the first story title and display it in this Text Widget.
                     storyBrain.getStory(),
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                       fontSize: 25.0,
                     ),
@@ -52,7 +53,12 @@ class _StoryPageState extends State<StoryPage> {
               ),
               Expanded(
                 flex: 2,
-                child: FlatButton(
+                child: TextButton(
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.red)))),
                   onPressed: () {
                     //Choice 1 made by user.
                     //TODO: Step 18 - Call the nextStory() method from storyBrain and pass the number 1 as the choice made by the user.
@@ -61,7 +67,7 @@ class _StoryPageState extends State<StoryPage> {
                       storyBrain.nextStory(1);
                     });
                   },
-                  color: Colors.red,
+                  //color: Colors.red,
                   child: Text(
                     //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
                     storyBrain.getChoice1(),
@@ -80,7 +86,13 @@ class _StoryPageState extends State<StoryPage> {
                 //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
                 child: Visibility(
                   visible: storyBrain.buttonShouldBeVisible(),
-                  child: FlatButton(
+                  child: TextButton(
+                    style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(18.0),
+                                    side: BorderSide(color: Colors.red)))),
                     onPressed: () {
                       //Choice 2 made by user.
                       //TODO: Step 19 - Call the nextStory() method from storyBrain and pass the number 2 as the choice made by the user.
@@ -88,7 +100,7 @@ class _StoryPageState extends State<StoryPage> {
                         storyBrain.nextStory(2);
                       });
                     },
-                    color: Colors.blue,
+                    // color: Colors.blue,
                     child: Text(
                       //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
                       storyBrain.getChoice2(),
